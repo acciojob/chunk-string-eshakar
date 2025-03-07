b58c56d072d022d344f22cb2385d42ab8af16526
eshakar <102459513+eshakar@users.noreply.github.com>
2025-03-07 21:49:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function stringChop(str, size) {
-  // your code here
+  // your code here  
+	if (!str) {
+    return [];
+  }
+
+  let resultArray = [];
+
+  while (str.length > 0) {
+
+    if (str.length > size) {
+ 
+      resultArray.push(str.substring(0, size));
+   
+      str = str.substring(size);
+    } else {
+      
+      resultArray.push(str);
+      str = '';
+    }
+  }
+
+  return resultArray;
 }
 
 // Do not change the code below
